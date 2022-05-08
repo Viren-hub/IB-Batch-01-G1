7c1060681929b5e4d25e4a0cb65d4b4fa9a4cd2b
Komalpati <89268474+Komalpati@users.noreply.github.com>
2022-05-08 22:29:49 +0530

    I have add div ,css properties in html files

--- covidAssignment/corona1.js
+++ covidAssignment/corona1.js
@@ -1,3 +1,38 @@
+function calConfirmedCases() {
+    var table = document.getElementById("tableId");
+    var totalConfirmed = 0;
+    for (var i = 1; i < table.rows.length; i++) {
+        totalConfirmed = totalConfirmed + parseInt(table.rows[i].cells[8].innerHTML);
+    }
+    document.getElementById("b1").innerHTML = "ConfirmedTotal =" + totalConfirmed;
+}
+
+function calcTotalDeaths() {
+    var table = document.getElementById("tableId");
+    var totalDeaths = 0;
+    for (var i = 1; i < table.rows.length; i++) {
+        totalDeaths = totalDeaths + parseInt(table.rows[i].cells[9].innerHTML);
+    }
+    document.getElementById("b2").innerHTML = "DeathsTotal =" + totalDeaths;
+}
+
+function calcTotalRecovered() {
+    var table = document.getElementById("tableId");
+    var totalRecoverd = 0;
+    for (var i = 1; i < table.rows.length; i++) {
+        totalRecoverd = totalRecoverd + parseInt(table.rows[i].cells[10].innerHTML);
+    }
+    document.getElementById("b3").innerHTML = "RecoveredTotal =" + totalRecoverd;
+}
+
+function calcTotalActiveCases() {
+    var table = document.getElementById("tableId");
+    var totalActiveCases = 0;
+    for (var i = 1; i < table.rows.length; i++) {
+        totalActiveCases = totalActiveCases + parseInt(table.rows[i].cells[11].innerHTML);
+    }
+    document.getElementById("b4").innerHTML = "ActiveTotal =" + totalActiveCases;
+}
 
 
 function getddl(name) {
